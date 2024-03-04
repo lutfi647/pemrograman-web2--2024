21bbd7a598b8ce4b9663e5e3d47c2af39fde1136
Lutfi fuat Azhar <149373019+lutfi647@users.noreply.github.com>
2024-03-04 15:36:06 +0700

    Add files via upload

---
+++ praktikum3/proses_registrasi.php
@@ -0,0 +1,9 @@
+<?php 
+
+$domisili = ["Jakarta", "Bogor", "Depok", "Tanggerang", "Sukajaya"];
+
+$program_studi = [
+    "SI" => "Sistem Informasi", 
+    "TI" => "Teknik Informatika",
+    "BD" => "Bisnis Digital"
+];
